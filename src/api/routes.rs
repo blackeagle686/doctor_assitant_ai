262e89b22b87974b6a26f5388220b5bc93ee8b72
blackeagle686 <mathematecs1@gmail.com>
2026-07-14 19:07:29 +0300

    Update [routes.rs (pub fn create_router)]

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -42,7 +42,7 @@ pub fn create_router() -> Router {
 pub struct AppState{
     pub embedding: Arc<EmbeddingService>,
     pub vdb: Arc<QdrantDb> ,
-
+    pub llm: Option<T>
 }
 
 /// Endpoint: POST /recognize

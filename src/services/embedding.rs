0d45ca1842148d5086846ac40f5d3b3b77175def
blackeagle686 <mathematecs1@gmail.com>
2026-07-14 18:46:52 +0300

    Update [embedding.rs]

---
+++ src/services/embedding.rs
@@ -0,0 +1,36 @@
+use anyhow::Result;
+use fastembed::{
+    EmbeddingModel,
+    InitOptions,
+    TextEmbedding,
+};
+
+pub struct EmbeddingService {
+    model: TextEmbedding,
+}
+
+impl EmbeddingService {
+    pub fn new() -> Result<Self> {
+        let model = TextEmbedding::try_new(
+            InitOptions::new(EmbeddingModel::AllMiniLML6V2)
+        )?;
+
+        Ok(Self { model })
+    }
+
+    pub fn embed(&self, text: &str) -> Result<Vec<f32>> {
+        let embeddings = self.model.embed(
+            vec![text.to_string()],
+            None,
+        )?;
+
+        Ok(embeddings.into_iter().next().unwrap())
+    }
+
+    pub fn embed_batch(
+        &self,
+        texts: Vec<String>,
+    ) -> Result<Vec<Vec<f32>>> {
+        Ok(self.model.embed(texts, None)?)
+    }
+}
